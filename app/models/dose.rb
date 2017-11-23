b405e70243468b306b02975867361bd0e495e5d0
Margaux COSTA <mgx.costa@gmail.com>
2017-11-23 15:41:40 +0100

    model + validation + controller = ok

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,17 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  # A dose must have a description
+  validates :description, presence: true
+
+  # A dose must have a cocktail
+  validates :cocktail_id, presence: true
+
+  # A dose must have a ingredient
+  validates :ingredient_id, presence: true
+
+  # [cocktail, ingredient] pairings should be unique
+  validates :cocktail_id, uniqueness: {scope: :ingredient_id}
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, dependent: :destroy
+  has_many :doses
+  # has_many :doses, through: :ingredients
+
+  # An ingredient must have a unique name
+  validates :name, uniqueness: true
+
 end
